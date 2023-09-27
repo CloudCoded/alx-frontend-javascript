bdbdb17783bb1a0f38f1af59e4b997a56386ff9b
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-09-27 16:11:39 +0000

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,26 +1,12 @@
-import axios from 'axios';
-
 function getResponseFromAPI() {
-  // Replace these variables with your actual API endpoint and request parameters
-  const apiUrl = 'https://api.example.com/your-endpoint';
-  const requestData = {
-    // Your request data here
-  };
-
   return new Promise((resolve, reject) => {
-    axios
-      .post(apiUrl, requestData)
-      .then((response) => {
-        // Check if the API response contains the data you need
-        if (response && response.data) {
-          resolve(response.data); // Resolve with API response data
-        } else {
-          reject('Error: API response does not contain data');
-        }
-      })
-      .catch((error) => {
-        reject(`Error: API call failed - ${error.message}`);
-      });
+    // API call logic here
+    // ...
+    if (responseFromAPI) {
+      resolve(responseFromAPI); // resolve with API response
+    } else {
+      reject("Error: API call failed"); // reject with error message
+    }
   });
 }
 
