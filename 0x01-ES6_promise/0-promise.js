95b2ec4d684c55613f7aa73cf88ea17e85dc50d5
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-09-27 16:04:38 +0000

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,13 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // Simulate an asynchronous API request (replace this with your actual API call)
-    setTimeout(() => {
-      const success = true; // Simulate a successful response
-      if (success) {
-        const response = { data: 'API response data' }; // Replace with your API response
-        resolve(response); // Resolve the Promise with the response data
-      } else {
-        const error = new Error('API request failed'); // Create an error object
-        reject(error); // Reject the Promise with the error
-      }
-    }, 1000); // Simulate a 1-second delay (adjust as needed)
+    // API call logic here
+    // ...
+    if (responseFromAPI) {
+      resolve(responseFromAPI); // resolve with API response
+    } else {
+      reject("Error: API call failed"); // reject with error message
+    }
   });
 }
+
+export default getResponseFromAPI;
