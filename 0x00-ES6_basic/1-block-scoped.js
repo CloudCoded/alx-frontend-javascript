b769cc2b000ba4b3064af3cb5c72f09e7e2db9a2
CloudCoded <abdulazeezakindolapo@gmail.com>
2023-09-27 14:07:37 +0000

    hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
 taskBlock()
 
 export default function taskBlock() {
-  if(trueOrFalse){
-     var task = true;
-     var task2 = false;
-  
- }
-  return [task, task2]
+ var task;
+ var task2
+ if(trueOrFalse){
+ task = true;
+  task2 = false;
+}
+ return [task, task2]
 } 
